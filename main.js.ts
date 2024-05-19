54bcb4f69c5acf27798fc4054f87ea6e2f835a7d
PhysicsWizard <55866895+PhysicsWizard@users.noreply.github.com>
2024-05-19 21:14:54 +1000

    PixelShader
    -- PIxelates the texture correctly

--- main.js.ts
+++ main.js.ts
@@ -1,5 +1,8 @@
 ﻿import * as THREE from "three"
 import {string} from "three/examples/jsm/nodes/shadernode/ShaderNode";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,50 +10,73 @@ camera.position.z = 100;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+const orbitControls = new OrbitControls(camera, renderer.domElement);
 const colour1: THREE.Vector3 = new THREE.Vector3( 1, 0.392, 0.945 );
 const colour2: THREE.Vector3 = new THREE.Vector3( 0.929, 1, 0);
 const scale1: THREE.Vector3 = new THREE.Vector3( 1,1,1);
 const scale2: THREE.Vector3 = new THREE.Vector3( 5, 5, 5);
 const sceneObjects = [];
+const textureLoader = new THREE.TextureLoader();
+let shaderMat: THREE.ShaderMaterial;
 
 
 let _VS: string  = `
 uniform vec3 scale;
+varying vec2 vUv;
 void main(){
-gl_Position = projectionMatrix * modelViewMatrix * vec4(position * scale, 1.0 );
+vUv = uv;
+gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0 );
 }
 `;
 let _FS: string = `
-uniform vec3 cubeColour;
+uniform sampler2D mainTex;
+uniform vec2 resolution;
+uniform float pixelSize;
+varying vec2 vUv;
 void main(){
-gl_FragColor = vec4(cubeColour, 1.0);}`;
+vec2 pixelatedUV = floor(vUv * resolution /pixelSize) * pixelSize/resolution;
+vec4 col = texture2D(mainTex, pixelatedUV);
+gl_FragColor = col;}`;
+const waterTexture =  textureLoader.load( "./Textures/rainbowTex.jpg", function( texture){
+    texture.minFilter = THREE.NearestFilter;
+    texture.magFilter = THREE.NearestFilter;
+    texture.wrapS = THREE.ClampToEdgeWrapping;
+    texture.wrapT = THREE.ClampToEdgeWrapping;
+    shaderMat = new THREE.ShaderMaterial({
+        uniforms: {
+            cubeColour: {value: new THREE.Vector3(1.0, 0.0,0.0)},
+            scale: {value: new THREE.Vector3(1.0, 1.0, 1.0)},
+            mainTex: {value: texture},
+            resolution: {value: new THREE.Vector2(texture.image.width, texture.image.height)},
+            pixelSize: {value: 2},
+        },
+        vertexShader: _VS,
+        fragmentShader: _FS,
+    })
+    InitSceneObjects(shaderMat);
+} );
 
-const shaderMat: THREE.ShaderMaterial = new THREE.ShaderMaterial({
-    uniforms: {
-        cubeColour: {value: new THREE.Vector3(1.0, 0.0,0.0)},
-        scale: {value: new THREE.Vector3(1.0, 1.0, 1.0)},
-    },
-    vertexShader: _VS,
-    fragmentShader: _FS,
-})
-
-let boxGeometry = new THREE.BoxGeometry(20,20,20,10,10,10);
-let sphereMat1 = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-});
-let sphereMat2 = new THREE.MeshBasicMaterial({
-    color: 0x0000ff,
-});
-let sphereMesh1 = new THREE.Mesh(boxGeometry, shaderMat);
-sphereMesh1.position.set(-50,0,0);
-scene.add(sphereMesh1);
-sceneObjects.push(sphereMesh1);
+function InitSceneObjects(shaderMat){
+    let boxGeometry = new THREE.BoxGeometry(20,20,20,10,10,10);
+    let sphereMat1 = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+    });
+    let sphereMat2 = new THREE.MeshBasicMaterial({
+        color: 0x0000ff,
+    });
+    let sphereMesh1 = new THREE.Mesh(boxGeometry, shaderMat);
+    sphereMesh1.position.set(-50,0,0);
+    scene.add(sphereMesh1);
+    sceneObjects.push(sphereMesh1);
 
 
-let sphereMesh2 = new THREE.Mesh(boxGeometry, sphereMat2);
-sphereMesh2.position.set(50,0,0);
-scene.add(sphereMesh2);
-sceneObjects.push(sphereMesh2);
+    let sphereMesh2 = new THREE.Mesh(boxGeometry, sphereMat2);
+    sphereMesh2.position.set(50,0,0);
+    scene.add(sphereMesh2);
+    sceneObjects.push(sphereMesh2);
+}
+
+
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
@@ -72,9 +98,8 @@ function animate() {
     }
     const interpolatedColour = new THREE.Vector3().lerpVectors(colour1,colour2,t);
     const interpolatedShape = new THREE.Vector3().lerpVectors(scale1,scale2,t);
-    shaderMat.uniforms.cubeColour.value.copy(interpolatedColour); 
-    shaderMat.uniforms.scale.value.copy(interpolatedShape);
-    console.log(typeof t,t);
+    //shaderMat.uniforms.cubeColour.value.copy(interpolatedColour);
+    //shaderMat.uniforms.scale.value.copy(interpolatedShape);
     for(let object of sceneObjects) {
         object.rotation.x += 0.01
         object.rotation.y += 0.01
