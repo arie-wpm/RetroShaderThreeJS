ff79adcd7717c0872a2fca2fec9bdbe7d7bc9a9d
Arie Maljers <ariemaljers@gmail.com>
2024-05-20 11:40:12 +1000

    Dither Added
    --Dither is Added

--- main.js.ts
+++ main.js.ts
@@ -35,11 +35,44 @@ let _FS: string = `
 uniform sampler2D mainTex;
 uniform vec2 resolution;
 uniform float pixelSize;
+uniform int bayerLevel;
+uniform float spread;
+uniform int redColourCount;
+uniform int blueColourCount;
+uniform int greenColourCount;
 varying vec2 vUv;
+const int bayer2[2 * 2] = int[](0, 2, 3, 1);
+const int bayer4[4 * 4] = int[](0, 8, 2, 10, 12, 4, 14, 6, 3, 11, 1, 9, 15, 7, 13, 5);
+const int bayer8[8 * 8] = int[](0, 32, 8, 40, 2, 34, 10, 42, 48, 16, 56, 24, 50, 18, 58, 26, 12, 44, 4, 36, 14, 46, 6, 38, 60, 28, 52, 20, 62, 30, 54, 22, 3, 35, 11, 43, 1, 33, 9, 41, 51, 19, 59, 27, 49, 17, 57, 25, 15, 47, 7, 39, 13, 45, 5, 37, 63, 31, 55, 23, 61, 29, 53, 21);
+float GetBayer2(int x, int y) {
+    return float(bayer2[int((x % 2)), int((y % 2) * 2)]) * (1.0 / 4.0) - 0.5;
+}
+float GetBayer4(int x, int y) {
+    return float(bayer4[int((x % 4)), int((y % 4) * 4)]) * (1.0 / 16.0) - 0.5;
+}
+
+float GetBayer8(int x, int y) {
+    return float(bayer8[int((x % 8)), int((y % 8) * 8)]) * (1.0 / 64.0) - 0.5;
+}
 void main(){
 vec2 pixelatedUV = floor(vUv * resolution /pixelSize) * pixelSize/resolution;
 vec4 col = texture2D(mainTex, pixelatedUV);
-gl_FragColor = col;}`;
+int x = int(resolution.x);
+int y = int(resolution.y);
+float bayerValues[3] = float[3](0.0,0.0,0.0);
+bayerValues[0] = GetBayer2(int(x),int(y));
+bayerValues[1] = GetBayer4(int(x),int(y));
+bayerValues[2] = GetBayer8(int(x),int(y));
+
+vec4 bayerOutput = col + spread * bayerValues[bayerLevel];
+bayerOutput.rgb /= max(vec3(1.0), bayerOutput.rgb);
+
+bayerOutput.r = floor((float(redColourCount) - 1.0) * bayerOutput.r + 0.5) / (float(redColourCount) - 1.0);
+bayerOutput.g = floor((float(greenColourCount) - 1.0) * bayerOutput.g + 0.5) / (float(greenColourCount) - 1.0);
+bayerOutput.b = floor((float(blueColourCount) - 1.0) * bayerOutput.b + 0.5) / (float(blueColourCount) - 1.0);
+
+gl_FragColor = bayerOutput;}
+`;
 const waterTexture =  textureLoader.load( "./Textures/rainbowTex.jpg", function( texture){
     texture.minFilter = THREE.NearestFilter;
     texture.magFilter = THREE.NearestFilter;
@@ -52,6 +85,11 @@ const waterTexture =  textureLoader.load( "./Textures/rainbowTex.jpg", function(
             mainTex: {value: texture},
             resolution: {value: new THREE.Vector2(texture.image.width, texture.image.height)},
             pixelSize: {value: 64},
+            bayerLevel: {value: 0},
+            spread: {value: 0.3},
+            redColourCount: {value: 4},
+            greenColourCount: {value: 4},
+            blueColourCount: {value: 4},
         },
         vertexShader: _VS,
         fragmentShader: _FS,
@@ -67,6 +105,11 @@ function CreatePixelShaderMaterial(){
             mainTex: {value: null},
             resolution: {value: new THREE.Vector2(1,1)},
             pixelSize: {value: 64},
+            bayerLevel: {value: 0},
+            spread: {value: 0.2},
+            redColourCount: {value: 4},
+            greenColourCount: {value: 4},
+            blueColourCount: {value: 4},
         },
         vertexShader: _VS,
         fragmentShader: _FS,
@@ -93,7 +136,7 @@ function InitSceneObjects(shaderMat){
     sceneObjects.push(sphereMesh2);
 }
 
-gltfLoader.load( "./Models/silent_ash/scene.gltf", function( gltf ){
+gltfLoader.load( "./Models/bulbasaur/scene.gltf", function( gltf ){
     rabbitModel = gltf.scene;
     rabbitModel.traverse((o) => {
         if (o.isMesh && o.material.map){
@@ -103,7 +146,7 @@ gltfLoader.load( "./Models/silent_ash/scene.gltf", function( gltf ){
                 o.material.map.image.width,
                 o.material.map.image.height
             );
-            objectShaderMat.uniforms.pixelSize.value = 16;
+            objectShaderMat.uniforms.pixelSize.value = 6;
             o.material = objectShaderMat;
         }
     });
@@ -112,7 +155,7 @@ gltfLoader.load( "./Models/silent_ash/scene.gltf", function( gltf ){
     gltf.scene.position.set(-50,20,0);
 });
 
-gltfLoader.load( "./Models/silent_ash/scene.gltf", function( gltf ){
+gltfLoader.load( "./Models/bulbasaur/scene.gltf", function( gltf ){
     rabbitModel2 = gltf.scene;
     scene.add(gltf.scene);
     gltf.scene.scale.set(30,30,30);
